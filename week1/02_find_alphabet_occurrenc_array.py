355e8a178fded32ef2af41a277cde2e0058f012c
fnvkd5316 <fnvkd5316@gmail.com>
2022-05-15 17:22:22 +0900

    basic_algorithm

---
+++ week1/02_find_alphabet_occurrenc_array.py
@@ -0,0 +1,11 @@
+def find_alphabet_occurrence_array(string):
+    alphabet_occurrence_array = [0] * 26
+
+    for alphabet in string:
+        if alphabet.isalpha():
+            alphabet_occurrence_array[ord(alphabet) - ord('a')] += 1
+
+    return alphabet_occurrence_array
+
+
+print(find_alphabet_occurrence_array("hello my name is sparta"))
