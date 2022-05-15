161971b1c9783ff9b2e83c30b7bc6f03e6354b17
fnvkd5316 <fnvkd5316@gmail.com>
2022-05-15 17:24:07 +0900

    bublle_sort

---
+++ bubble_sort_basic.js
@@ -0,0 +1,36 @@
+function solution(arr, divisor) {
+    var answer = [];
+    
+    for(let i = 0; i < arr.length; i++){
+        if( arr[i] % divisor === 0){
+            answer.push(arr[i])
+        }
+    }
+    
+    // if (answer.length === 0) {
+    //     answer.push(-1)
+    // }else{
+    //     answer.sort((a, b)=>(a-b))
+    // }
+    // answer.length === 0 ? answer.push(-1) : answer.sort((a, b)=>(a-b))
+    
+    answer.length === 0 ? answer.push(-1) : my_sort(answer)
+    
+    return answer
+}
+
+function my_sort(arr){
+    
+    for (let i = 0; i < arr.length-1; i++){
+        for (let j = 0; j < arr.length-1-i; j++){
+            if( arr[j] > arr[j+1]){
+                // let temp = arr[j]
+                // arr[j] = arr[j+1]
+                // arr[j+1] = temp 
+                
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]] //구조분해 할당 javascript 기능!!
+                
+            }
+        }
+    }
+}
