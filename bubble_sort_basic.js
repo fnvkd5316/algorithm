039512c7d4ad109662ba160524e587f5ecb5b314
fnvkd5316 <fnvkd5316@gmail.com>
2022-08-20 18:27:13 +0900

    add log

--- bubble_sort_basic.js
+++ bubble_sort_basic.js
@@ -23,7 +23,7 @@ function my_sort(arr){
     
     for (let i = 0; i < arr.length-1; i++){
         for (let j = 0; j < arr.length-1-i; j++){
-            if( arr[j] > arr[j+1]){
+            if (arr[j] > arr[j+1]) {
                 // let temp = arr[j]
                 // arr[j] = arr[j+1]
                 // arr[j+1] = temp 
---
+++ week1/04_arr_11.js
@@ -0,0 +1,35 @@
+// nums는 숫자 배열, 모든 경우의 수를 출력하세요. 순열 알고리즘
+
+
+function aa ( nums ) {
+
+  if ( nums.length === 1 ) {
+    return nums;
+  }
+
+  const result = [];
+
+
+
+  for (let i = 0; i < nums.length; i++) {
+    
+    temp_arr.push(nums[i]);
+
+    console.log(temp_arr);
+  }
+
+  return result;
+}
+
+function bb ( nums, size, ) {
+
+
+
+
+  return ;
+}
+
+
+const nums = [1, 2, 3];
+
+console.log( aa(nums) );
